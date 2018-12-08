5910a60cb37f3c013ded8e393e562b0efc4e7232
vaaPo <ripraptiptop@gmail.com>
2018-12-08 13:21:50 +0200

    init for hw3.1

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const repl = require('repl');
 const bodyParser = require('body-parser');
 let nos=require('./datafiles/Notes/notes');
+let pers=require('./datafiles/PhoneBook/persons');
+
 let notes=nos;
+let persons=pers;
 
 //let notes=require('./datafiles/Notes/notes');
 //import persons from './datafiles/PhoneBook';
@@ -12,29 +15,9 @@ app.use(bodyParser.json())
 
 
 console.log('hello world');
+console.log('notes',notes);
+console.log('persons',persons);
 
- /**
-  let notes = [
-    {
-      id: 1,
-      content: 'HTML on helppoa',
-      date: '2017-12-10T17:30:31.098Z',
-      important: true
-    },
-    {
-      id: 2,
-      content: 'Selain pystyy suorittamaan vain javascriptiä',
-      date: '2017-12-10T18:39:34.091Z',
-      important: false
-    },
-    {
-      id: 3,
-      content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
-      date: '2017-12-10T19:20:14.298Z',
-      important: true
-    }
-  ];
- */
 
   app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
