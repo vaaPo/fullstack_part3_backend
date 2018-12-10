e9c39b4b93a621ae9910771f4cccf701709cd76e
vaaPo <ripraptiptop@gmail.com>
2018-12-10 18:20:55 +0200

    solution candidate to hw3.19x - result trick and promise chaining

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ if (process.argv.length===5) {
 //      console.log("pers",pers);
 //      console.log("persons",persons);
     });
+
     app.get('/api/persons', (request, response) => {
       modelspersons.Person
       .find({}, {__v: 0})
@@ -152,11 +153,39 @@ if (process.argv.length===5) {
       if (body.name === undefined) {
         return response.status(400).json({error: 'name missing'});
       };
-    
-      const person = new modelspersons.Person({
-        name: body.name,
-        phonenumber: body.phonenumber,
+      console.log('body.name:',body.name);
+      let nameToBeAdded = body.name;
+      //https://stackoverflow.com/questions/9660587/do-something-if-nothing-found-with-find-mongoose
+      modelspersons.Person
+      .find({name: body.name}) //body.name})  "Arto Vihavainen"
+      .then(result => {
+        if (!result.length) {// this is wanted position, lets add
+          console.log('result',result);
+          console.log('we didnt find duplicate in mongodb');
+          const person = new modelspersons.Person({
+            name: body.name,
+            phonenumber: body.phonenumber,
+          });
+          person                  //FIXME promise chain v2 for person post
+          .save()
+          .then(modelspersons.formatPerson) 
+          .then(savedAndFormattedPerson => {
+            response.json(savedAndFormattedPerson)
+          })
+          .catch(error => {
+            //console.log(error)
+            response.status(400).send({ error: 'something went royally wrong with your post' });  // bad request
+          });
+        } else { 
+          console.log('post person conflict error 409, name exists already',body.name);
+          console.log('result',result);
+          response.status(409).send({ error: 'conflict error 409, name exists already'}); // request is conflict error 409, name exists
+        };
+      })
+      .catch(error => {
+        response.status(400).send({ error: 'problem in post person ' });  // bad request
       });
+
 
 /**     person
         .save()
@@ -168,16 +197,6 @@ if (process.argv.length===5) {
           response.status(400).send({ error: 'something went royally wrong with your post' });  // bad request
         });
     */
-        person                  //FIXME promise chain v2 for person post
-        .save()
-        .then(modelspersons.formatPerson) 
-        .then(savedAndFormattedPerson => {
-          response.json(savedAndFormattedPerson)
-        })
-        .catch(error => {
-          //console.log(error)
-          response.status(400).send({ error: 'something went royally wrong with your post' });  // bad request
-        });
   
 
     }); //app.post('/api/person'
@@ -336,7 +355,7 @@ app.put('/api/notes/:id', (request, response) => {
         })
         .catch(error => {
           response.status(400).send({ error: 'malformatted id' });  // bad request
-        })
+        });
     }); //app.get('/api/notes/:id'
     app.delete('/api/notes/:id', (request, response) => {
       console.log('app.delete /api/notes/:id',request.params.id);
